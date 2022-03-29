3b11d49cc91170bc7521f26175d85fb4bd9d6b27
alexamaingard <maingard.alexa@gmail.com>
2022-03-29 11:01:57 +0200

    add client routes setup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { LOCAL } from './config/paths';
+import { HomePage } from './components/HomePage';
+import { SignUp } from './components/SignUp';
+import { SignIn } from './components/SignIn';
+import { MyProfile } from './components/MyProfile';
+import { MyLogs } from './components/MyLogs';
+import { RecordAttack } from './components/RecordAttack';
+
 
 export const App = () => {
     return (
         <div className='App'>
+            <Routes>
+                <Route path={LOCAL.HOMEPAGE} element={<HomePage />} />
+                <Route
+                    path={LOCAL.SIGN_UP} element={<SignUp />}
+                />
+                <Route
+                    path={LOCAL.SIGN_IN} element={<SignIn />}
+                />
+                <Route
+                    path={LOCAL.PROFILE} element={<MyProfile />}
+                />
+                <Route 
+                    path={LOCAL.LOGS} element={<MyLogs />} 
+                />
+                <Route
+                    path={LOCAL.RECORD_ATTACK} element={<RecordAttack />}
+                />
+            </Routes>
         </div>
     );
 }
---
+++ src/client/config/paths.ts
@@ -0,0 +1,10 @@
+class LOCAL {
+    static readonly HOMEPAGE: '/'
+    static readonly SIGN_IN: '/signIn'
+    static readonly SIGN_UP: '/signUp'
+    static readonly PROFILE: '/profile'
+    static readonly LOGS: '/logs'
+    static readonly RECORD_ATTACK: '/recordAttack'
+}
+
+export { LOCAL }
