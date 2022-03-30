eac12318260fdf7ece9e5adc774842d47b38d800
alexamaingard <maingard.alexa@gmail.com>
2022-03-30 12:36:20 +0200

    add interfaces: Intensity, Medication, Type, Symptom, Trigger, Attack

---
+++ src/server/config/interfaces.ts
@@ -0,0 +1,26 @@
+export interface Intensity {
+    number: number, 
+    level: string
+}
+
+export interface Medication {
+    drug: string,
+    dose: string,
+    type: string
+}
+
+export interface Type {
+    name: string
+}
+
+export interface Symptom {
+    name: string
+}
+
+export interface Trigger {
+    name: string
+}
+
+export interface Attack {
+    
+}
