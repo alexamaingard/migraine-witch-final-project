0c518d0b0c70cb79a3f50fb6c41400dc95a52a62
alexamaingard <maingard.alexa@gmail.com>
2022-03-30 12:38:07 +0200

    add debugging printing statements

--- src/server/index.ts
+++ src/server/index.ts
@@ -19,12 +19,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(ROUTES.USER, userRouter);
 app.use(ROUTES.ATTACK, attackRouter);
 
+// console.log('user:', ROUTES.USER, 'attack:', ROUTES.ATTACK);
+
 app.get('*', (req, res) => {
+    // console.log(req);
     res.json({ ok: true });
 });
 
+// app.post('*', (req, res) => {
+//     console.log(req);
+//     res.json({ ok: true });
+// });
+
 const port = process.env.PORT || 4000;
+const url = process.env.REACT_APP_API_URL;
+
+// console.log('attack url:', url + ROUTES.ATTACK);
+// console.log('user url:', url + ROUTES.USER);
 
 app.listen(port, () => {
-    console.log(`\n Server is running on http://localhost:${port}\n`);
+    console.log(`\n Server is running on ${url}\n`);
 });
