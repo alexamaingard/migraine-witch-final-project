917695f89e371c54a3bd3328381348f6e443e14d
alexamaingard <maingard.alexa@gmail.com>
2022-03-30 19:00:02 +0200

    add attack routes

--- src/server/routers/attack.ts
+++ src/server/routers/attack.ts
@@ -1,37 +1,39 @@
 import { Router } from 'express';
 
 import { 
+    createAttack,
+    createAuraByUser,
+    createEffectByUser,
     createMedicationByUser,
     createNote,
+    createPhysicalLocationByUser,
+    createReliefMethodByUser,
     createSymptomByUser,
     createTriggerByUser,
     createTypeByUser,
     deleteNote,
     initDataBase, 
-    testFunc, 
     updateNote
 } from '../controllers/attack';
 
 const router = Router();
 
-router.get('', testFunc);
+//Tested
+router.post('/init', initDataBase);
 
-console.log('connected function', initDataBase);
+//Not tested yet
+router.post('/med', createMedicationByUser);
+router.post('/type', createTypeByUser);
+router.post('/symptom', createSymptomByUser);
+router.post('/trigger', createTriggerByUser);
+router.post('/phyLoc', createPhysicalLocationByUser);
+router.post('/effect', createEffectByUser);
+router.post('/aura', createAuraByUser);
+router.post('/relief', createReliefMethodByUser);
+router.post('/note', createNote);
+router.patch('/note/:noteId', updateNote);
+router.delete('/note/:noteId', deleteNote);
 
-router.post('init/', initDataBase);
-
-
-
-
-
-
-router.post('med/', createMedicationByUser);
-router.post('type/', createTypeByUser);
-router.post('symptom/', createSymptomByUser);
-router.post('trigger/', createTriggerByUser);
-
-router.post('note/', createNote);
-router.patch('note/', updateNote);
-router.delete('note/', deleteNote);
+router.post('/', createAttack);
 
 export default router;
