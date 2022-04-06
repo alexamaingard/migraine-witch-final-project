e1b0a4844c20835c2ff6397c9ffc07a0061bdb0c
alexamaingard <maingard.alexa@gmail.com>
2022-04-06 13:30:45 +0200

    add contact form functionality

--- src/client/components/Contact.tsx
+++ src/client/components/Contact.tsx
@@ -1,31 +1,54 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+import { EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_USER_ID } from '../config/emailJS'
+
 import '../styles/contact.css';
 
 export const Contact = () => {
+    const form = useRef();
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>):Promise<void> => {
+        event.preventDefault();
+
+        try {
+            const result = await emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, form.current, EMAILJS_USER_ID);
+            console.log(result.text);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
     return (
         <div className='contact'>
             <h2>Do you have questions or suggestions?</h2>
             <div className='contact-info'>
                 <div className='contact-form-container'>
-                    <form className='contact-form'>
+                    <form ref={form} className='contact-form' onSubmit={handleSubmit}>
                         <label htmlFor=''>
-                            First name:
-                            <input type='text' />
-                        </label>
-                        <label htmlFor=''>
-                            Last name:
-                            <input type='text' />
+                            Name:
+                            <input 
+                                type='text' 
+                                name='from_name'
+                                required
+                            />
                         </label>
                         <label htmlFor=''>
                             Email:
-                            <input type='text' />
+                            <input 
+                                type='email' 
+                                name='reply_to'
+                                required
+                            />
                         </label>
                         <label htmlFor=''>
                             Your message:
                             <textarea
-                                name=''
-                                id=''
+                                name='message'
                                 cols={30}
                                 rows={5}
+                                required
                             ></textarea>
                         </label>
                         <div className='button-container'>
